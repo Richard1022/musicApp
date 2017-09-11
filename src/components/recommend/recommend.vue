9c6dc72db73358698f2756490d750b91a996eb9a
Richard <717388657@qq.com>
2017-09-11 18:31:19 +0800

    2017/9/11 remomend slider 1

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,19 +1,39 @@
 <template>
 	<div class="remomend">
-		remomend
+			<div class="recomend-content">
+				<div class="recomend-wrapper">
+						<slider>
+							<div v-for="item in recomends">
+								<a :href="item.linkUrl">
+									<img :src="item.picUrl">
+								</a>
+							</div>
+						</slider>
+				</div>
+			</div>
 	</div>
 </template>
 
 <script>
   import {getRecommend} from 'api/recommend'  // 引入getremonmend方法 获取推荐页数据
   import {ERR_OK} from 'api/config'  // 引入常规常量参数
-  
+	import slider from 'base/slider' // 引入slider基础轮播组件
+	
 	export default{
+		data(){
+			return{
+				recomends:[],
+			}
+		},
+		components:{
+			slider,
+		},
 		methods:{
 		  _getRecommend(){
 		    getRecommend().then((res)=>{
 		      if(res.code===ERR_OK){
-		        console.log(res.data);
+						
+						this.recomends=res.data.slider;
 		      }
 		    })
 		  }
