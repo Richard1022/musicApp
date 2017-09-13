4ed1710fd8ba788670ad47607bbbec0a08288c5c
Richard <717388657@qq.com>
2017-09-13 18:18:12 +0800

    singer.js

---
+++ src/api/singer.js
@@ -0,0 +1,7 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams,options} from './config.js'
+//引入jsonp及jsonp常规公共参数
+
+export function getSingerList(){
+    const url='https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+}
